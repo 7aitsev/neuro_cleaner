749c408f323b61cd6c5d87c748a5ef7f231f0c9c
Maxim Zaitsev <7aitsev@gmail.com>
2015-12-13 01:35:08 +0300

    Initial commit

---
+++ app/src/main/java/org/appspot/neurostorage/TabControl.java
@@ -0,0 +1,23 @@
+package org.appspot.neurostorage;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class TabControl extends Fragment {
+
+  public TabControl() {
+  }
+
+  @Override
+  public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                           Bundle savedInstanceState) {
+    View rootView = inflater.inflate(R.layout.control, container, false);;
+    return rootView;
+  }
+}
---
+++ app/src/main/java/org/appspot/neurostorage/TabExclusions.java
@@ -0,0 +1,23 @@
+package org.appspot.neurostorage;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class TabExclusions extends Fragment {
+
+  public TabExclusions() {
+  }
+
+  @Override
+  public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                           Bundle savedInstanceState) {
+    View rootView = inflater.inflate(R.layout.exclusions, container, false);;
+    return rootView;
+  }
+}
